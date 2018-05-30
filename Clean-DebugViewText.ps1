b9ae3f0447426d9bcd6f3d45e4a021b0eff93de6
carrollh <carrollh@email.sc.edu>
2018-05-30 11:56:30 -0400

    changes from home

--- Clean-DebugViewText.ps1
+++ Clean-DebugViewText.ps1
@@ -6,14 +6,16 @@ Param(
 $text = Get-Content -Path $Path
 
 for ($i = 0; $i -lt $text.Length; $i++) { 
-    $text[$i] = $text[$i].Substring(20).TrimStart() 
+    if($text[$i].Length -gt 26) {
+        $text[$i] = $text[$i].Substring(26).TrimStart() 
+    }
 }
 
 $output = [System.Collections.ArrayList]@()
-
+$text
 $s = "OUTPUT"
 $text | foreach {
-    if ( ($_ -Like "ExtMirr:*") -Or ($_ -Like ">>*") ) {
+    if ( ($_ -Like "ExtMirr:*") -Or ($_ -Like ">>*") -Or ($_ -Like "L: * - ") ) {
         $output.Add($s) > $Null
         $s = $_.Trim()
     } else { 
