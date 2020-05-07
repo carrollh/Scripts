84595f1eb02421c98df3086ac40bda36801aee7b
carrollh <heath.aaron.carroll@gmail.com>
2020-05-07 09:10:25 -0400

    updates to template deployment scripts for DKCE support

--- AWS/TemplateDev/Get-DeploymentStatus.ps1
+++ AWS/TemplateDev/Get-DeploymentStatus.ps1
@@ -8,39 +8,70 @@
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$True, ValueFromPipeline, Position=0)]
-    [System.Collections.HashTable]  $Stacks = $Null,
+    [Parameter(Mandatory=$False, ValueFromPipeline)]
+    [System.Collections.Specialized.OrderedDictionary] $Stacks,
+
+    [System.Collections.HashTable] $StackNames = $Null,
 
     [Parameter(Mandatory=$False, Position=1)]
-    [string]   $Profile = $Null
+    [string] $Profile = $Null
 )
 
 $results = [System.Collections.HashTable]@{}
-$i = 0
-foreach ($region in $Stacks.Keys) {
-    $i += 1
-    $result = [System.Collections.ArrayList]@()
 
-    if($Profile) {
-        $rootStack = Get-CFNStack -Region $region -StackName $Stacks[$region] -ProfileName $Profile
-        $result.Add($rootStack) > $Null
-        Get-CFNStack -Region $region -ProfileName $Profile | Where ParentId -like $Stacks[$region] | %{
-            $result.Add($_) > $Null
-        }
-    }
-    else {
-        $rootStack = Get-CFNStack -Region $region -StackName $Stacks[$region]
-        $result.Add($rootStack) > $Null
-        Get-CFNStack -Region $region | Where ParentId -like $Stacks[$region] | %{
-            $result.Add($_) > $Null
-        }
-    }
+if($StackNames) {
+    $i = 0
+    foreach ($region in $StacksNames.Keys) {
+        $i += 1
+        $result = [System.Collections.ArrayList]@()
 
-    $results.Add($region, $result)
-    Write-Progress -Activity "Querying AWS" -Status ("Regions Completed: ($i/" + $Stacks.Keys.Count + ")") -PercentComplete ($i / $Stacks.Keys.Count*100)
+        if($Profile) {
+            $rootStack = Get-CFNStack -Region $region -StackName $StacksNames[$region] -ProfileName $Profile
+            $result.Add($rootStack) > $Null
+            Get-CFNStack -Region $region -ProfileName $Profile | Where ParentId -like $StacksNames[$region] | %{
+                $result.Add($_) > $Null
+            }
+        }
+        else {
+            $rootStack = Get-CFNStack -Region $region -StackName $StacksNames[$region]
+            $result.Add($rootStack) > $Null
+            Get-CFNStack -Region $region | Where ParentId -like $StacksNames[$region] | %{
+                $result.Add($_) > $Null
+            }
+        }
+
+        $results.Add($region, $result)
+        Write-Progress -Activity "Querying AWS" -Status ("Regions Completed: ($i/" + $Stacks.Keys.Count + ")") -PercentComplete ($i / $Stacks.Keys.Count*100)
+    }
+}
+else {
+    $i = 0
+    foreach ($region in $Stacks.Keys) {
+        $i += 1
+        $result = [System.Collections.ArrayList]@()
+
+        if($Profile) {
+            $rootStack = Get-CFNStack -Region $region -StackName $Stacks[$region][$region] -ProfileName $Profile
+            $result.Add($rootStack) > $Null
+            Get-CFNStack -Region $region -ProfileName $Profile | Where ParentId -like $Stacks[$region][$region] | %{
+                $result.Add($_) > $Null
+            }
+        }
+        else {
+            $rootStack = Get-CFNStack -Region $region -StackName $Stacks[$region][$region]
+            $result.Add($rootStack) > $Null
+            Get-CFNStack -Region $region | Where ParentId -like $Stacks[$region][$region] | %{
+                $result.Add($_) > $Null
+            }
+        }
+
+        $results.Add($region, $result)
+        Write-Progress -Activity "Querying AWS" -Status ("Regions Completed: ($i/" + $Stacks.Keys.Count + ")") -PercentComplete ($i / $Stacks.Keys.Count*100)
+    }
 }
 
 $results.Keys | %{
     Write-Host $_ 
     $results[$_] | Format-Table | Out-Host
 }
+
--- AWS/TemplateDev/Test-AWSDKCE.ps1
+++ AWS/TemplateDev/Test-AWSDKCE.ps1
@@ -22,21 +22,21 @@ Param(
     [string]   $Profile = "dev"
 )
 
-$masterStacks = [ordered]@{}
+$masterStacks = [ArrayList]@()
 
-$masterStacks.Add("ap-northeast-2",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-SQL-PAYG -OSVersion WS2012R2 -SQLServerVersion 2014SP1 -AMIType PAYG -Region ap-northeast-2 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("ap-south-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-SQL-BYOL -OSVersion WS2012R2 -SQLServerVersion 2014SP1 -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region ap-south-1 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("ap-southeast-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-NoSQL-PAYG -OSVersion WS2012R2 -SQLServerVersion None -AMIType PAYG -Region ap-southeast-1 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("ap-southeast-2",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-NoSQL-BYOL -OSVersion WS2012R2 -SQLServerVersion None -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region ap-southeast-2 -Branch develop -Profile $Profile -Verbose))
+$masterStacks.Add((.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-SQL-PAYG -OSVersion WS2012R2 -SQLServerVersion 2014SP1 -AMIType PAYG -Region ap-northeast-2 -Branch develop -Profile $Profile -Verbose))
+$masterStacks.Add((.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-SQL-BYOL -OSVersion WS2012R2 -SQLServerVersion 2014SP1 -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region ap-south-1 -Branch develop -Profile $Profile -Verbose))
+$masterStacks.Add((.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-NoSQL-PAYG -OSVersion WS2012R2 -SQLServerVersion None -AMIType PAYG -Region ap-southeast-1 -Branch develop -Profile $Profile -Verbose))
+$masterStacks.Add((.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2012R2-NoSQL-BYOL -OSVersion WS2012R2 -SQLServerVersion None -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region ap-southeast-2 -Branch develop -Profile $Profile -Verbose))
 
-$masterStacks.Add("ca-central-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-SQL-PAYG -OSVersion WS2016 -SQLServerVersion 2014SP1 -AMIType PAYG -Region ca-central-1 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("eu-central-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-SQL-BYOL -OSVersion WS2016 -SQLServerVersion 2014SP1 -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region eu-central-1 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("us-east-2",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-NoSQL-PAYG -OSVersion WS2016 -SQLServerVersion None -AMIType PAYG -Region us-east-2 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("eu-west-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-NoSQL-BYOL -OSVersion WS2016 -SQLServerVersion None -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region eu-west-1 -Branch develop -Profile $Profile -Verbose))
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-SQL-PAYG -OSVersion WS2016 -SQLServerVersion 2014SP1 -AMIType PAYG -Region ca-central-1 -Branch develop -Profile $Profile -Verbose)
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-SQL-BYOL -OSVersion WS2016 -SQLServerVersion 2014SP1 -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region eu-central-1 -Branch develop -Profile $Profile -Verbose)
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-NoSQL-PAYG -OSVersion WS2016 -SQLServerVersion None -AMIType PAYG -Region us-east-2 -Branch develop -Profile $Profile -Verbose)
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2016-NoSQL-BYOL -OSVersion WS2016 -SQLServerVersion None -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region eu-west-1 -Branch develop -Profile $Profile -Verbose)
 
-$masterStacks.Add("eu-west-2",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-SQL-PAYG -OSVersion WS2019 -SQLServerVersion 2014SP1 -AMIType PAYG -Region eu-west-2 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("eu-west-3",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-SQL-BYOL -OSVersion WS2019 -SQLServerVersion 2014SP1 -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region eu-west-3 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("sa-east-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-NoSQL-PAYG -OSVersion WS2019 -SQLServerVersion None -AMIType PAYG -Region sa-east-1 -Branch develop -Profile $Profile -Verbose))
-$masterStacks.Add("us-east-1",(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-NoSQL-BYOL -OSVersion WS2019 -SQLServerVersion None -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region us-east-1 -Branch develop -Profile $Profile -Verbose))
+$masterStacks.Add((.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-SQL-PAYG -OSVersion WS2019 -SQLServerVersion 2014SP1 -AMIType PAYG -Region eu-west-2 -Branch develop -Profile $Profile -Verbose))
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-SQL-BYOL -OSVersion WS2019 -SQLServerVersion 2014SP1 -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region eu-west-3 -Branch develop -Profile $Profile -Verbose)
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-NoSQL-PAYG -OSVersion WS2019 -SQLServerVersion None -AMIType PAYG -Region sa-east-1 -Branch develop -Profile $Profile -Verbose)
+#$masterStacks.Add(.\Test-AWSDKCETemplate.ps1 -Stackname HAC-DK-2019-NoSQL-BYOL -OSVersion WS2019 -SQLServerVersion None -AMIType BYOL -SIOSLicenseKeyFtpURL $SIOSLicenseKeyFtpURL -Region us-east-1 -Branch develop -Profile $Profile -Verbose)
 
 return $masterStacks
