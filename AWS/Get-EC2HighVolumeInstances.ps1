403a64c7849fe3e0fd4c9c66a58e8aa8b9841c21
carrollh <heath.aaron.carroll@gmail.com>
2021-03-04 18:13:01 -0500

    tweaks to aws cost reduction analysis scripts

--- AWS/Get-EC2HighVolumeInstances.ps1
+++ AWS/Get-EC2HighVolumeInstances.ps1
@@ -37,15 +37,24 @@ Param(
     [string[]] $Regions = @("ap-east-1","ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-north-1","eu-west-1","eu-west-2","eu-west-3","me-south-1","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2")
 )
 
+Write-Verbose "Get-EC2HighVolumeInstances - START"
+Write-Verbose "`tParameters ($($PsBoundParameters.Values.Count)):"
+$PsBoundParameters.Keys | foreach {
+    Write-Verbose "`t$_ = $($PsBoundParameters[$_])"
+}
+$outputTable = [Ordered]@{}
 foreach ($region in $Regions) {
+    Write-Verbose "`t$region"
     $volumeSummary = (.\Get-EC2VolumeSummary.ps1 -Profile $Profile -Region $region) | Sort -Property "VolumeSize" -Descending
     $instanceSummary = .\Get-EC2InstanceSummary.ps1 -Profile $Profile -Region $region
 
     $instances = $Null
     if($Profile) {
+        Write-Verbose "---`taws ec2 describe-instances --region $region --profile $Profile --output json"
         $instances = (& "aws" ec2 describe-instances --region $region --profile $Profile | ConvertFrom-Json).Reservations.Instances
     }
     else {
+        Write-Verbose "---`taws ec2 describe-instances --region $region --output json"
         $instances = (& "aws" ec2 describe-instances --region $region | ConvertFrom-Json).Reservations.Instances
     }
 
@@ -57,8 +66,12 @@ foreach ($region in $Regions) {
         }
         $instanceSummary[$region][$i] | Add-Member -MemberType NoteProperty -Name "TotalVolumeSize" -Value $size
     }
+
+    $outputTable.Add($region, ($instanceSummary[$region] | Sort -Property "TotalVolumeSize" -Descending)) > $Null
 }
 
-# spit out the desired instances sorted by most amount of storage first
+Write-Verbose "Get-EC2HighVolumeInstances - END"
+
+# spit out the desired instances sorted by most amount of storage first (per region)
 # unfortunately we may have volumes that are unaccounted for
-$instanceSummary[$region] | Sort -Property "TotalVolumeSize" -Descending
+return $outputTable
--- AWS/Get-EC2InstanceSummary.ps1
+++ AWS/Get-EC2InstanceSummary.ps1
@@ -20,16 +20,26 @@ Param(
     [string[]] $Regions = @("ap-east-1","ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-north-1","eu-west-1","eu-west-2","eu-west-3","me-south-1","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2")
 )
 
+Write-Verbose "`tGet-EC2InstanceSummary - START"
+Write-Verbose "`t`tParameters ($($PsBoundParameters.Values.Count)):"
+$PsBoundParameters.Keys | foreach {
+    Write-Verbose "`t`t$_ = $($PsBoundParameters[$_])"
+}
 $instanceTable = [Ordered]@{}
 foreach ($region in $Regions) {
+    Write-Verbose "`t`t$region"
     $instances = $Null
     $tags = $Null
     if($Profile) {
+        Write-Verbose "---`taws ec2 describe-instances --region $region --profile $Profile --output json"
         $instances = (& "aws" ec2 describe-instances --region $region --profile $Profile --output json | convertfrom-json).Reservations.Instances
+        Write-Verbose "---`taws ec2 describe-tags --region $region --profile $Profile --filters `"Name=resource-type,Values=instance`" `"Name=key,Values=Name`" --output json"
         $tags = (& "aws" ec2 describe-tags --region $region --profile $Profile --filters "Name=resource-type,Values=instance" "Name=key,Values=Name" --output json | convertfrom-json).Tags
     }
     else {
+        Write-Verbose "---`taws ec2 describe-instances --region $region --output json"
         $instances = (& "aws" ec2 describe-instances --region $region --output json | convertfrom-json).Reservations.Instances
+        Write-Verbose "---`taws ec2 describe-tags --region $region --filters `"Name=resource-type,Values=instance`" `"Name=key,Values=Name`" --output json"
         $tags = (& "aws" ec2 describe-tags --region $region --filters "Name=resource-type,Values=instance" "Name=key,Values=Name" --output json | convertfrom-json).Tags
     }
 
@@ -52,5 +62,6 @@ foreach ($region in $Regions) {
     $instanceTable.Add($region, ($instanceInfo | Sort-Object -Property LaunchTime)) > $Null
 }
 
+Write-Verbose "`tGet-EC2InstanceSummary - END"
 return $instanceTable
 # END
