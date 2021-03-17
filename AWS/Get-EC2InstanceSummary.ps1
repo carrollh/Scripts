3d2195bdc23b070f034b925a4c62da5cb178ca18
carrollh <heath.aaron.carroll@gmail.com>
2021-03-17 14:10:09 -0400

    better examples

--- AWS/Get-EC2InstanceSummary.ps1
+++ AWS/Get-EC2InstanceSummary.ps1
@@ -1,15 +1,29 @@
 # Get-EC2InstanceSummary.ps1
 #
-# Example:
-# $rs = @("ap-east-1","ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-north-1","eu-west-1","eu-west-2","eu-west-3","me-south-1","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2")
-# $outfile = "C:\Users\hcarroll\Desktop\AWS_Summary"
-# foreach($p in $ps){ 
-#     $ec2info = .\Get-EC2InstanceSummary.ps1 -Profile $p
-#     foreach($r in $rs){
-#         "`n$r" >> "$outfile\${p}_instances"
-#         echo $ec2info["$r"] | ft >> "$outfile\${p}_instances"
-#     }
-# }
+# Example 1 (super simple):
+#   $instanceSummary = .\Get-EC2InstanceSummary.ps1 -Profile "dev" -Regions "ca-central-1"
+#
+# Example 2 (for use with other scripts or saving as json doc):
+#   $ht = @{}
+#   $ps = @("dev","currentgen")
+#   $rs = @("ca-central-1","us-east-1","us-east-2","us-west-1","us-west-2")
+#   foreach($p in $ps){ 
+#       $profileInfo = .\Get-EC2InstanceSummary.ps1 -Profile $p -Regions $rs
+#       $ht.Add($p, $profileInfo)
+#   }
+#   $ht | ConvertTo-Json -Depth 10 | Out-File -Encoding utf8 -FilePath "C:\Users\hcarroll\Desktop\AWS_Summary.txt"
+#
+# Example 3 (for easy to read output in a file; edit $outfile FIRST):
+#   $ps = @("dev","currentgen")
+#   $rs = @("ca-central-1","us-east-1","us-east-2","us-west-1","us-west-2")
+#   $outfile = "C:\Users\hcarroll\Desktop\AWS_Summary"
+#   foreach($p in $ps){ 
+#       $ec2info = .\Get-EC2InstanceSummary.ps1 -Profile $p
+#       foreach($r in $rs){
+#           "`n$r" >> "$outfile\${p}_instances"
+#           echo $ec2info["$r"] | ft >> "$outfile\${p}_instances"
+#       }
+#   }
 
 [CmdletBinding()]
 Param(
