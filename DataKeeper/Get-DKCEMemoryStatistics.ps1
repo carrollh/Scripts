d6ee60a7d9c17060874f4aa5b97c85a355e8a99a
carrollh <heath.aaron.carroll@gmail.com>
2021-12-16 14:18:03 -0500

    new script for tracking memory

---
+++ DataKeeper/Get-DKCEMemoryStatistics.ps1
@@ -0,0 +1,74 @@
+# Get-DKCEMemoryStatistics.ps1
+
+[CmdletBinding()]
+param(
+    [int] $SleepSeconds = 60,
+    [int] $MaxSecondsToRun = 600
+)
+
+$ht = [Ordered]@{}
+$proclist = @('ExtMirrSvc', 'EMTray', 'MMC', 'RHS')
+$elapsedTime = 0
+$initialProcs = [System.Collections.ArrayList]@()
+while ($elapsedTime -lt $MaxSecondsToRun) {
+
+    # get this iteration's stats
+    $procs = [System.Collections.ArrayList]@()
+    foreach ($proc in $proclist) {
+        $processes = get-process $proc | Sort-Object -Property Id
+        foreach ($process in $processes) {
+            $procs.Add($process) | Out-Null 
+        }
+    }
+    $date = Get-Date
+    $ht.Add($date,$procs)
+
+    # display this iteration's stats
+    Write-Host "#######################################`n$($date)`nCurrent Memory Usage:"
+    $procs | FT Name, Id, VM, NPM, PM, WS, PrivateMemorySize, Handles
+
+    if($elapsedTime -eq 0) {
+        $initialProcs = $procs
+    }
+
+    if($elapsedTime -gt 0) {
+
+        # figure out the delta since the start of the script
+        $deltas = [System.Collections.ArrayList]@()
+        foreach ($procname in $proclist) {
+            $ids = ($procs | Where-Object { $_.ProcessName -Like "$procname" }).Id
+            foreach ($id in $ids) {
+                $oldproc = $initialProcs | Where-Object -Property Id -eq $id
+                $newProc = $procs | Where-Object -Property Id -eq $id
+                $delta = [PSCustomObject]@{
+                    Name = $procname
+                    Id = $id
+                    VM = ($newproc.VM - $oldproc.VM)
+                    NPM = ($newproc.NPM - $oldproc.NPM)
+                    PM = ($newproc.PM - $oldproc.PM)
+                    WS = ($newproc.WS - $oldproc.WS)
+                    PrivateMemorySize = ($newproc.PrivateMemorySize - $oldproc.PrivateMemorySize)
+                    Handles = ($newproc.Handles - $oldproc.Handles)
+                }
+                $deltas.Add($delta) | Out-Null
+            }
+        }
+
+        # display the deltas
+        Write-Host "Change since start of script:"
+        $deltas | FT Name, Id, VM, NPM, PM, WS, PrivateMemorySize, Handles
+    }
+
+    if($elapsedTime -lt ($MaxSecondsToRun - $SleepSeconds)) {
+        Write-Host "Sleeping for $SleepSeconds seconds..."
+        Start-Sleep $SleepSeconds
+    }
+
+    $elapsedTime += $SleepSeconds
+}
+
+                    #NPM = (($ht[0] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).NPM - ($ht[($ht.Count-1)] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).NPM)
+                    #PM = (($ht[0] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).PM - ($ht[($ht.Count-1)] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).PM)
+                    #WS = (($ht[0] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).WS - ($ht[($ht.Count-1)] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).WS)
+                    #PrivateMemorySize = (($ht[0] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).PrivateMemorySize - ($ht[($ht.Count-1)] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).PrivateMemorySize)
+                    #Handles = (($ht[0] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).Handles - ($ht[($ht.Count-1)] | Where-Object { ($_.ProcessName -like "$proc") -And ($_.Id -eq $id) }).Handles)
